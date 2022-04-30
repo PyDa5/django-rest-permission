6272d16375b9f7adcd6fc5f989e5f19849c3b1fd
PyDa5 <88498830+PyDa5@users.noreply.github.com>
2022-04-30 19:28:36 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
 
-# 配置auth_permission表中的codename字段格式
-PERMISSION_CODENAME_PATTERN = 'view://{app_name}/{view_group}/{permission_code}'
-
-# 配置django_content_type表中的app_label格式
-APP_LABEL_PATTERN = 'drf_{app_name}'
-
-# 配置视图权限分组名称
-VIEW_GROUP_PROP = 'view_group'
-
-# 配置访问视图所需权限标的属性
-VIEW_ACCESS_PERMISSIONS_PROP = 'view_access_permissions'
+# 缓存权限键样式
+DEFUALT_USER_PERMISSIONS_CACHE_KEY_PATTERN = 'drp_user:permissions:{user_id}'
